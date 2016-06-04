88a0a4590b9d049015cc6437cb814b259d167285
Christoph Herthel <me@hchristo.de>
2016-06-04 20:28:48 +0200

    Test

---
+++ app/src/main/java/app/mailjunky/de/MainActivity.java
@@ -0,0 +1,21 @@
+package app.mailjunky.de;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webView);
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://m.mailjunky.de");
+    }
+
+}
---
+++ app/src/main/java/app/mailjunky/de/MyAppWebViewClient.java
@@ -0,0 +1,22 @@
+package app.mailjunky.de;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Christoph on 04.06.2016.
+ */
+public class MyAppWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if(Uri.parse(url).getHost().endsWith("html5rocks.com")) {
+            return false;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;
+    }
+}
