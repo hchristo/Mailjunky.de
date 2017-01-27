d14322ba1b25b8e0daeaec2c44b695fc0d4b0458
Christoph Herthel <hchristo@freenet.de>
2017-01-27 20:16:30 +0100

    Add Splashscreen Add Fixed Rotate

--- app/src/main/java/app/mailjunky/de/MainActivity.java
+++ app/src/main/java/app/mailjunky/de/MainActivity.java
@@ -2,7 +2,6 @@ package app.mailjunky.de;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -16,10 +15,10 @@ public class MainActivity extends AppCompatActivity {
         // Enable Javascript
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        mWebView.loadUrl("https://m.mailjunky.de");
+        mWebView.loadUrl("https://m.mailjunky.de/");
 // Stop local links and redirects from opening in browser instead of WebView
         mWebView.setWebViewClient(new MyAppWebViewClient());
-        CookieManager.getInstance().setAcceptCookie(true);
+
     }
 
 }
